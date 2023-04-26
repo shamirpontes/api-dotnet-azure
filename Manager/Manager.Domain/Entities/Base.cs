87758882d294d74f69d3ce88c3878fa8b6fc5a92
Shamir Pontes <shamirpontes77@gmail.com>
2023-04-26 20:19:27 -0300

    cirando domínio rico e validações para o usuário

--- Manager/Manager.Domain/Entities/Base.cs
+++ Manager/Manager.Domain/Entities/Base.cs
@@ -9,5 +9,11 @@ namespace Manager.Domain.Entities
     public abstract class Base 
     {
         public long Id { get; set; }
+
+        internal List<string> _errors;
+
+        public IReadOnlyCollection<string> Errors => _errors;
+
+        public abstract bool Validate();
     }
 }
--- Manager/Manager.Domain/Entities/User.cs
+++ Manager/Manager.Domain/Entities/User.cs
@@ -1,10 +1,66 @@
-﻿using System;
+﻿using Manager.Domain.Validators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Manager.Domain.Entities {
-    internal class User {
+namespace Manager.Domain.Entities 
+{
+    public class User : Base
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        //EF
+        protected User()
+        {
+
+        }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        public void ChangeName(string name) 
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password) 
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email) 
+        {
+            Email = email;
+            Validate();
+        }
+
+        public override bool Validate() 
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid) 
+            {
+                foreach(var error in validation.Errors) 
+                {
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inválidos, por favor corrija-os.", _errors[0]);
+                }
+            }
+        }
     }
 }
